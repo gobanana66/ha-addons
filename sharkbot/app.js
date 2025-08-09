5a8fd1d4360cec423caa24c0c77c96aeb3ab16dd
anna <anna@corefour.com>
2025-08-09 12:55:32 -0400

    add sharkbot

---
+++ sharkbot/app.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const app = express();
+const port = 3000;
+
+// Load secrets from secrets.yaml
+const secrets = yaml.load(fs.readFileSync('secrets.yaml', 'utf8'));
+
+
+const { exec } = require("child_process");
+const knope = require('knope');
+
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const token = secrets.token;
+const prefix = secrets.prefix;
+
+// Create a new client instance
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+});
+
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+  console.log('Ready!');
+  client.user.setActivity({
+       type: "PLAYING",
+       name: "Silly Shark Things",
+    });
+});
+
+const shark = 'Shark! ';
+const poopEmoji = '💩';
+
+client.on('message', message => {
+  if (message.author.bot) return; //returns doesn't respond to bot or webhook messages.
+  const times = Math.floor(Math.random() * 9 + 3);
+  if (message.content.toLowerCase().includes('shark')) {
+    message.react('🦈');
+    message.channel.send(shark.repeat(times)); // do anything here
+  }
+  if (message.content.toLowerCase().includes('queen')) {
+    message.react('👑');
+  }
+  if (message.content.includes('Doug Ford')) {
+    message.react('💩');
+  }
+  if (message.content.toLowerCase().includes('stunning')) {
+    message.react('🥃');
+  }
+  if (message.content.toLowerCase().includes('bosh')) {
+    message.channel.send('https://i.redd.it/xol8lpzdvo471.gif');
+  }
+  if (message.content.toLowerCase().includes('jail')) {
+    message.channel.send('https://c.tenor.com/rlyqxiRiunEAAAAC/tenor.gif');
+  }
+  if (message.content.startsWith(prefix)) {
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    if (command === 'hype') {
+      let compliment = knope.getCompliment(args, 'random');
+      message.channel.send(`${compliment}`);
+    } else if (command === 'chant') {
+      let chant = args[0].charAt(0).toUpperCase() + args[0].slice(1) + '! ';
+      message.channel.send(chant.repeat(times + 3));
+    } else if (command === 'stunning') {
+      message.channel.send(`*Negroni sbagliato oooh*`);
+    }
+  }
+
+});
+
+// Login to Discord with your client's token
+client.login(token).catch(console.error)
+client.on('debug', console.log);
+client.on("debug", async info => {
+  if (info.includes("429")) {
+    exec("kill 1")
+  }
+})
---
+++ sharkbot/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv bashio
+set +u
+
+
+bashio::log.info "Starting bridge service."
+npm run start
